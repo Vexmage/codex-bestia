4cc6d88728b0a9a7c7af56d704a0e96bd11c6a68
Joel Southall <vextechmage@gmail.com>
2025-08-10 23:06:11 -0700

    fix: route imports to new standalone component file names

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MonstersListComponent } from './features/monsters-list/monsters-list';
+import { MonsterDetailComponent } from './features/monster-detail/monster-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'monsters', pathMatch: 'full' },
+  { path: 'monsters', component: MonstersListComponent },
+  { path: 'monsters/:slug', component: MonsterDetailComponent },
+  { path: '**', redirectTo: 'monsters' }
+];
